d27b5abf7b486b6942e973bcef79b32b7312aabd
nugrohoandy <30788849+nugrohoandy@users.noreply.github.com>
2017-08-18 06:38:24 +0700

    Add files via upload

---
+++ blogsafelink.js
@@ -0,0 +1,81 @@
+<div class="text-center margin-bottom-20">
+<h3>
+Advertise</h3>
+KODE IKLAN DISINI
+  </div>
+<div class="row margin-bottom-20">
+<div class="col-md-6">
+<div class="panel panel-primary">
+<div class="panel-heading text-center">
+<h2>
+<i class="fa fa-shield"></i> Safe Link Converter <i class="fa fa-shield"></i></h2>
+</div>
+<div class="panel-body text-center">
+<div class="text-primary margin-bottom-20">
+Encrypting your link and protect the link from viruses, malware, thief, etc!<br />Made your link safe to visit.</div>
+<div class="progress" id="daplong">
+<div class="progress-bar progress-bar-striped active six-sec-ease-in-out" data-transitiongoal="100" role="progressbar">
+</div>
+</div>
+<button class="alert alert-dismissible alert-success btn-lg" id="download2" style="display: none;"><i aria-hidden="true" class="fa fa-thumbs-o-up"></i> <b>Well done!</b> you have successfully gained access to Decrypted Link. <i aria-hidden="true" class="fa fa-hand-o-down slideInDownThumb"></i></button>
+
+        </div>
+</div>
+</div>
+<div class="col-md-6 text-left">
+<div class="panel panel-info">
+<!-- Default panel contents -->
+  <br />
+<div class="panel-heading">
+<h2>
+<i aria-hidden="true" class="fa fa-question-circle-o"></i> How to use our tool:</h2>
+</div>
+<div class="panel-body">
+<ol>
+<li>Click on <b>How To Use</b> menu above.</li>
+<li>Click on the code and <kbd><kbd>CTRL</kbd> + <kbd>C</kbd></kbd> on your keyboard.</li>
+<li>Paste the code in your HTML blog theme before the <mark>&lt;/body&gt;</mark>.</li>
+<li>Save your HTML blog theme. you are done!</li>
+<li>Now, your blog's outbound links was encrypted!</li>
+</ol>
+</div>
+</div>
+</div>
+<div class="clear">
+</div>
+</div>
+<div class="row margin-bottom-20">
+<div class="col-md-4 text-center">
+<h3>
+Advertise</h3>
+KODE IKLAN DISINI
+</div>
+<div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h3 class="panel-title">
+<b>Your link show here</b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
+</div>
+<div class="panel-body">
+<script src="" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://kliksafelinkconverter.blogspot.com/p/geneate.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<h3>
+Advertise</h3>
+KODE IKLAN DISINI
+</div>
+<div class="clear">
+</div>
+</div>
+<div class="container">
+<div class="text-left">
+</div>
+</div>
